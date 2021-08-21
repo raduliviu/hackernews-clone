d44b2dca26f8cfba4df39cefe1217811b1da023f
Erik <elaktrato@gmail.com>
2021-08-21 15:24:58 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,11 +16,7 @@ function App() {
 
   useEffect(
     () => {
-      getData('news');
-      let timer = setTimeout(() => setLoading(false), 2000);
-      return () => {
-        clearTimeout(timer);
-      }
+      getData('news')
     }, []
     )
 
@@ -30,7 +26,7 @@ function App() {
   const url = 'https://hn.algolia.com/api/v1/search?query='
 
   const getData = async (searchQuery) => {
-
+    setLoading(true)
     try {
       const response = await fetch(url + encodeURI(searchQuery), { cache: 'no-cache' })
       if (response.ok) {
@@ -40,10 +36,11 @@ function App() {
     } catch (error) {
       console.log(error)
     }
+    setLoading(false);
   }
 
   if(loading){
-    return <Loader />
+    return <Loader /> 
   }else return (
     
     <div className="App">
