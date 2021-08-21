3d7c6c50b493730a8f9ee842ad2fe1c28d762d1f
Radu Baltag <baltag.radu@gmail.com>
2021-08-21 13:48:23 +0200

    Added Search functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import {hackernewsData} from './hackernews.js'
-import React, { useState } from 'react';
+import { hackernewsData } from './hackernews.js'
+import React, { useEffect, useState } from 'react';
 import Header from './Header'
 import Main from './Main'
 
@@ -8,11 +8,34 @@ function App() {
   const [searchResults, setSearchResults] = useState(
     hackernewsData
   );
+
+  useEffect(
+    () => {
+      getData('news')
+    }, []
+  )
+
+  const url = 'https://hn.algolia.com/api/v1/search?query='
+
+  const getData = async (searchQuery) => {
+    try {
+      const response = await fetch(url + encodeURI(searchQuery), { cache: 'no-cache' })
+      if (response.ok) {
+        const jsonResponse = await response.json()
+        setSearchResults(jsonResponse)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className="App">
-      <Header />
-      <Main 
-        searchResults = {searchResults}
+      <Header
+        getData={getData}
+      />
+      <Main
+        searchResults={searchResults}
       />
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function Header () {
+function Header (props) {
+    const [searchTerm, setSearchTerm] = useState('')
+
+    const handleChange = (event) => {
+        setSearchTerm(event.target.value)
+    }
+
+    const handleEnterSubmit = (event) => {
+        if(event.key === "Enter"){
+            props.getData(searchTerm)
+        }
+    }
+
     return(
         <header>
             <p>Hacker News</p>
-            <p>Search:</p>
-            <input type="text"/>
+            <input 
+                onChange={handleChange} 
+                onKeyPress={handleEnterSubmit} 
+                id="searchField" 
+                type="text"
+                value={searchTerm}
+                />
+            <button onClick={() => props.getData(searchTerm)}>Search</button>
         </header>
     )
 }
