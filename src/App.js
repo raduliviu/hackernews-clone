42babfab5fff9a9cd38091aece72443988cb7b35
Erik <elaktrato@gmail.com>
2021-08-21 15:12:59 +0200

    Make Loading work and look well

--- src/App.js
+++ src/App.js
@@ -3,21 +3,34 @@ import { hackernewsData } from './hackernews.js'
 import React, { useEffect, useState } from 'react';
 import Header from './Header'
 import Main from './Main'
+import Loader from './Loader'
+
 
 function App() {
+  const[loading, setLoading] = useState( true )
+
+  
   const [searchResults, setSearchResults] = useState(
-    hackernewsData
-  );
+    ""
+    );
 
   useEffect(
     () => {
-      getData('news')
+      getData('news');
+      let timer = setTimeout(() => setLoading(false), 2000);
+      return () => {
+        clearTimeout(timer);
+      }
     }, []
-  )
+    )
+
+  
+
 
   const url = 'https://hn.algolia.com/api/v1/search?query='
 
   const getData = async (searchQuery) => {
+
     try {
       const response = await fetch(url + encodeURI(searchQuery), { cache: 'no-cache' })
       if (response.ok) {
@@ -29,7 +42,10 @@ function App() {
     }
   }
 
-  return (
+  if(loading){
+    return <Loader />
+  }else return (
+    
     <div className="App">
       <Header
         getData={getData}
