5d70cc180739473d91c839b79f276eba60723409
Radu Baltag <baltag.radu@gmail.com>
2021-08-28 16:39:56 +0200

    Styled sort component

--- src/Sort.js
+++ src/Sort.js
@@ -5,7 +5,7 @@ function Sort (props) {
     return(
         <div id="sortBar">
             <form>
-                <label htmlFor="sort-select">Sort By</label>
+                <label htmlFor="sort-select">Sort by: </label>
                 <select onChange={(e)=>{props.sortingToggle(e.target.value)}} name="sorting" id="sort-select">
                     <option value="search">Popularity</option>
                     <option value="search_by_date">Date</option>
